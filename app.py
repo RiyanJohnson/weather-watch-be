8d5fae737d2b7d01a7ca4a74ccb2bfafbcef78b4
RiyanJohnson <john.riyan16@gmail.com>
2026-09-06 16:13:31 +0530

    feat: connected auroradb

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
-from fastapi import FastAPI
-app=FastAPI()
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
+from db.db import engine, get_db, Base
+from db.models import Test
+
+app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+@app.post("/test")
+def add_test(name: str, db: Session = Depends(get_db)):
+    data = Test(name=name)
+
+    db.add(data)
+    db.commit()
+    db.refresh(data)
+
+    return data
 
 @app.get("/test")
-def test_route():
-    return {"test":"wassup gng"}
+def get_test(db: Session = Depends(get_db)):
+    return db.query(Test).all()
---
+++ db/test_db.py
@@ -0,0 +1,9 @@
+from db.db import engine
+
+try:
+    with engine.connect() as connection:
+        print("Database connected successfully!")
+
+except Exception as e:
+    print("Database connection failed:")
+    print(e)
